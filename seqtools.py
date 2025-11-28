31e32682cabfc932370a462ab572b41ae765eac2
Pavel Grobushkin <if_you_know@you_know.com>
2025-11-28 22:04:37 +0100

    Do homework 5

--- seqtools.py
+++ seqtools.py
@@ -3,14 +3,16 @@ Sequencing tools
 
 This module provides utility functions for basic bioinformatics tasks.
 Use run_dna_rna_tools() for various transformations of nucleotide sequences. 
-Use filter_fastq() to filter FASTQ reads by length, GC content, and quality.
+Use filter_fastq() to filter reads from FASTQ-files by length, GC content, and quality.
 
 Requirements:
 - typing (standard library)
 - statistics (standard library)
+- os (standard library)
 """
 
 from typing import Dict, Tuple, Union, List
+import os
 from seqtools_modules.dna_rna_tools import *
 from seqtools_modules.fastq_tools import *
 
@@ -56,30 +58,41 @@ def run_dna_rna_tools(*args: str) -> Union[str, bool, List[Union[str, bool]]]:
 
 
 def filter_fastq(
-    seqs: Dict[str, Tuple[str, str]],
+    input_fastq: str,
+    output_fastq: str,
     gc_bounds: Union[Tuple[float, float], float] = (0, 100),
-    length_bounds: Union[Tuple[int, int], int] = (0, 2**32),
+    length_bounds: Union[Tuple[int, int], int] = (0, 2 ** 32),
     quality_threshold: float = 0,
-) -> Dict[str, Tuple[str, str]]:
+) -> None:
     """
-    Filters FASTQ sequences based on GC-content, length, and quality score.
+    Filters FASTQ sequences in a on-the-fly manner from an input file based on GC-content,
+    length, and quality score, and saves the filtered records to a new output file.
 
     Arguments:
-    seqs: dict - Dictionary of  FASTQ sequences {name: (sequence, quality_string)}.
-    gc_bounds: tuple or float - Interval or upper bound for GC content (default (0, 100)).
-    length_bounds: tuple or int - Interval or upper bound for sequence length (default (0, 2**32)).
+    input_fastq: str - Path to the input FASTQ file.
+    output_fastq: str - Name of the output file (will be saved in 'filtered/' directory).
+    gc_bounds: tuple or float - Interval (min, max) or upper bound (max) for GC content (default (0, 100)).
+    length_bounds: tuple or int - Interval or upper bound (max) for sequence length (default (0, 2**32)).
     quality_threshold: float - Minimum average Phred33 quality score (default 0).
-
-    Returns:
-    dict - Filtered dictionary of sequences.
     """
-    output = {}
-    for read_id, (seq, qual) in seqs.items():
-        if (
-            quality_filter(qual, quality_threshold)
-            and gc_filter(seq, gc_bounds)
-            and len_filter(seq, length_bounds)
-        ):
-            output[read_id] = (seq, qual)
+    if not os.path.isdir("filtered"):
+        os.mkdir("filtered")
 
-    return output
+    with open(input_fastq, "r") as fastq_file, open(
+        os.path.join('filtered', output_fastq), "w"
+    ) as filtered_output:
+        while True:
+            head_line = fastq_file.readline()
+            if not head_line:
+                break
+            seq_line = fastq_file.readline()
+            sep_line = fastq_file.readline()
+            qual_line = fastq_file.readline()
+
+            sequence = seq_line.strip()
+            quality = qual_line.strip()
+
+            if check_filters(
+                sequence, quality, quality_threshold, gc_bounds, length_bounds
+            ):
+                filtered_output.writelines([head_line, seq_line, sep_line, qual_line])
